7a099f733be02ce5111e572c1a1f4596e3ce6512
Roelant Vos <roelant@ravos.nl>
2020-09-15 17:09:52 +1000

    Added GetModuleAreaByModuleId function.
    Returns Area based on ModuleId.

---
+++ 020_DIRECT_Framework/Direct_Framework/Functions/GetModuleAreaByModuleId.sql
@@ -0,0 +1,26 @@
+﻿CREATE FUNCTION omd.GetModuleAreaByModuleId
+(
+	@ModuleId INT -- The identifier of the Module (PK).
+)
+RETURNS VARCHAR(255) AS
+
+-- =============================================
+-- Function: Get Module Area (by Id)
+-- Description:	Takes the module id as input and returns the area code as registered in the framework
+-- =============================================
+
+BEGIN
+	-- Declare ouput variable
+
+	DECLARE @ModuleArea VARCHAR(255) = 
+	(
+	  SELECT module.AREA_CODE
+	  FROM omd.MODULE module 
+	  WHERE MODULE_ID = @ModuleId
+	)
+
+	SET @ModuleArea = COALESCE(@ModuleArea,'N/A')
+
+	-- Return the result of the function
+	RETURN @ModuleArea
+END
