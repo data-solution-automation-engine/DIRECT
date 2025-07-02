948d0f7539b81e52aa3bc93073e307e82f22ccb8
Stefan Johansson <stefan@stefanjohansson.org>
2025-07-02 22:58:20 +1000

    fix missing col rename
    and some minor updates

--- Direct_Framework.Integration.Tests/Tests/DemoSqlProcTests.cs
+++ Direct_Framework.Integration.Tests/Tests/DemoSqlProcTests.cs
@@ -3,10 +3,9 @@ using FluentAssertions;
 using System.Data.SqlClient;
 using Xunit;
 
-public class OrderProcedureTests : IClassFixture<MsSqlFixture>
+public class OrderProcedureTests(MsSqlFixture f) : IClassFixture<MsSqlFixture>
 {
-    readonly string _cs;
-    public OrderProcedureTests(MsSqlFixture f) => _cs = f.ConnectionString;
+    readonly string _cs = f.ConnectionString;
 
     [Fact]
     public async Task UpsertOrder_returns_zero_on_success()
