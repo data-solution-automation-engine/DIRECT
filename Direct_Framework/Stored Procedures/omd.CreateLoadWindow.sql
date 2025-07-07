e40bbd7c48eeb4622aff12d3352a611eed0669ff
Stefan Johansson <stefan@stefanjohansson.org>
2025-07-07 20:44:44 +1000

    Update omd.CreateLoadWindow.sql

--- Direct_Framework/Stored Procedures/omd.CreateLoadWindow.sql
+++ Direct_Framework/Stored Procedures/omd.CreateLoadWindow.sql
@@ -231,10 +231,20 @@ WHERE modinst.EXECUTION_STATUS_CODE = ''Succeeded''';
   SET @StartValue = CONVERT(NVARCHAR(MAX), @StartValueDate, 126);
   SET @EndValue = CONVERT(NVARCHAR(MAX), @EndValueDate, 126);
 
+  SET @SuccessIndicator = 'Y';
+
+  GOTO EndOfProcedure;
+
+  FailureEndOfProcedure:
+
+    SET @SuccessIndicator = 'N'
+    SET @LogMessage = N'' + @SpName + ' ended in failure.';
+    SET @MessageLog = [omd].[AddLogMessage](DEFAULT, DEFAULT, DEFAULT, @LogMessage, @MessageLog)
+
+  GOTO EndOfProcedure;
+
   -- End of procedure label
   EndOfProcedure:
-
-  SET @SuccessIndicator = 'Y'
 
   SET @EndTimestamp = SYSUTCDATETIME();
   SET @EndTimestampString = FORMAT(@EndTimestamp, 'yyyy-MM-dd HH:mm:ss.fffffff');
