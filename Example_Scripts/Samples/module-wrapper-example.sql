4a6372c1e6ec86cbead124ec6c27aa6afda09c44
Roelant Vos <roelant@ravos.nl>
2025-08-03 13:16:53 +0200

    Update module-wrapper-example.sql

--- Example_Scripts/Samples/module-wrapper-example.sql
+++ Example_Scripts/Samples/module-wrapper-example.sql
@@ -1,39 +1,39 @@
-/* 
+/*
   This example combines the registration, and possible reset, for a Module in DIRECT with the execution of specific code to be executed.
 */
 
-/* 
-  Parameters 
+/*
+  Parameters
 */
 
-DECLARE @Reset CHAR(1) = 'N';
+DECLARE @Reset CHAR(1) = 'Y';
 DECLARE @ModuleCode VARCHAR(255) = 'DataLogisticsExample';
-DECLARE @ModuleDescription VARCHAR(4000) =  'Data logistics Example';
+DECLARE @ModuleDescription VARCHAR(255) =  'Data logistics Example';
 DECLARE @ModuleType VARCHAR(255) = 'SQL';
 DECLARE @ModuleSourceDataObject VARCHAR(255) = 'N/A';
 DECLARE @ModuleTargetDataObject VARCHAR(255) = 'N/A';
 DECLARE @ModuleAreaCode VARCHAR(255) = 'INT';
 DECLARE @ModuleFrequency VARCHAR(255) = 'Continuous';
-DECLARE @ModuleInactiveIndicator VARCHAR(255) = 'N';
+DECLARE @ModuleActiveIndicator VARCHAR(255) = 'Y';
 
 DECLARE @Module_Id INT;
-SELECT @Module_Id = module_id FROM [900_Direct_Framework].[omd].module WHERE module_code = @ModuleCode;
+SELECT @Module_Id = MODULE_ID FROM [Direct_Framework].[omd].[MODULE] WHERE MODULE_CODE = @ModuleCode;
 
-/* 
-  Module Registration block.
+/*
+  Module Registration.
   This is often a separate, independent step.
 */
 
-INSERT INTO [900_Direct_Framework].[omd].MODULE (MODULE_CODE, MODULE_DESCRIPTION, MODULE_TYPE, DATA_OBJECT_SOURCE, DATA_OBJECT_TARGET, AREA_CODE, FREQUENCY_CODE, INACTIVE_INDICATOR)
+INSERT INTO [Direct_Framework].[omd].MODULE (MODULE_CODE, MODULE_DESCRIPTION, MODULE_TYPE, DATA_OBJECT_SOURCE, DATA_OBJECT_TARGET, AREA_CODE, FREQUENCY_CODE, ACTIVE_INDICATOR)
 SELECT *
-FROM 
+FROM
 (
-  VALUES (@ModuleCode, @ModuleDescription, @ModuleType, @ModuleSourceDataObject, @ModuleTargetDataObject,@ModuleAreaCode, @ModuleFrequency, @ModuleInactiveIndicator)    
+  VALUES (@ModuleCode, @ModuleDescription, @ModuleType, @ModuleSourceDataObject, @ModuleTargetDataObject,@ModuleAreaCode, @ModuleFrequency, @ModuleActiveIndicator)
 ) AS refData( MODULE_CODE, MODULE_DESCRIPTION, MODULE_TYPE, DATA_OBJECT_SOURCE, DATA_OBJECT_TARGET, AREA_CODE, FREQUENCY_CODE, INACTIVE_INDICATOR)
-WHERE NOT EXISTS 
+WHERE NOT EXISTS
 (
   SELECT NULL
-  FROM [900_Direct_Framework].[omd].MODULE module
+  FROM [Direct_Framework].[omd].MODULE module
   WHERE module.MODULE_CODE = refData.MODULE_CODE
 );
 
@@ -44,10 +44,10 @@ WHERE NOT EXISTS
 IF @Reset = 'Y'
   BEGIN TRY
     BEGIN
-      DELETE FROM [900_Direct_Framework].[omd].event_log WHERE MODULE_INSTANCE_ID IN (SELECT MODULE_INSTANCE_ID FROM [900_Direct_Framework].[omd].module_instance WHERE MODULE_ID=@Module_id)
-      DELETE FROM [900_Direct_Framework].[omd].source_control WHERE MODULE_INSTANCE_ID IN (SELECT MODULE_INSTANCE_ID FROM [900_Direct_Framework].[omd].module_instance WHERE MODULE_ID=@Module_id)
-      DELETE FROM [900_Direct_Framework].[omd].module_instance WHERE MODULE_ID=@Module_id
-      DELETE FROM [900_Direct_Framework].[omd].batch_module where module_id = @Module_id
+      DELETE FROM [Direct_Framework].[omd].[EVENT_LOG] WHERE MODULE_INSTANCE_ID IN (SELECT MODULE_INSTANCE_ID FROM [Direct_Framework].[omd].[MODULE_INSTANCE] WHERE MODULE_ID = @Module_id)
+      DELETE FROM [Direct_Framework].[omd].[SOURCE_CONTROL] WHERE MODULE_INSTANCE_ID IN (SELECT MODULE_INSTANCE_ID FROM [Direct_Framework].[omd].[MODULE_INSTANCE] WHERE MODULE_ID = @Module_id)
+      DELETE FROM [Direct_Framework].[omd].[MODULE_INSTANCE] WHERE MODULE_ID = @Module_id
+      DELETE FROM [Direct_Framework].[omd].[BATCH_MODULE] where MODULE_ID = @Module_id
     END
   END TRY
 
@@ -55,7 +55,7 @@ IF @Reset = 'Y'
     THROW
   END CATCH
 
-/* 
+/*
   Start a new Module Instance.
 */
 
@@ -63,8 +63,14 @@ BEGIN
 
   EXEC [omd].[RunModule]
     @ModuleCode = @ModuleCode,
-	@Debug = 'Y',
-	@Query = 'SELECT GETDATE()'
+	  @Debug = 'Y',
+	  @Query = 'SELECT GETDATE()'
 
 END
 GO
+
+/* Control block for reviewing the tests
+SELECT * FROM omd.MODULE
+SELECT * FROM omd.MODULE_INSTANCE
+SELECT * FROM omd.MODULE_INSTANCE_EXECUTED_CODE WHERE CHECKSUM='0xB776F34A647256234B172290D258C16E837CB980E4017898265A1B62806840D9BADF77AC518DC6CA1D33862ED9681F22CAA2BF45EB3DA97F8E346A5E75EDCEF6'
+*/
